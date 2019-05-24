eb23921c0515e97d2d9c18d29ed24634ff0bde9c
Khristian Punzalan <khrispunzalan@gmail.com>
2019-05-24 12:17:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,11 +16,11 @@ def speak_to_grandma (phrase)
 if phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
 elsif phrase == "#{phrase}"
-  puts "HUH?! SPEAK UP, SONNY!"
+  return "HUH?! SPEAK UP, SONNY!"
 elsif phrase == phrase.upcase
-  puts "NO, NOT SINCE 1938!"
+  return "NO, NOT SINCE 1938!"
 else
-  puts "HUH?! SPEAK UP, SONNY!"
+  return "HUH?! SPEAK UP, SONNY!"
 end
 
 end
